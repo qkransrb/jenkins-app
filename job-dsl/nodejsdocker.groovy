22c3e8354a1ddc7b866fe6979d9704bf7206b7e1
qkransrb <qkransrb90@gmail.com>
2023-03-07 22:25:18 +0900

    add job-dsl script file

---
+++ job-dsl/nodejsdocker.groovy
@@ -0,0 +1,26 @@
+job("nodejs docker example") {
+    scm("https://github.com/qkransrb/jenkins-app.git") { node ->
+        node / gitConfigName("qkransrb")
+        node / gitConfigEmail("qkransrb90@gmail.com")
+    }
+
+    triggers {
+        scm("H/S * * * *")
+    }
+
+    wrappers {
+        nodejs("nodejs")
+    }
+
+    steps {
+        dockerBuildAndPush {
+            repositoryName("qkransrb90/app")
+            tag("${GIT_REVISON,length=9}")
+            registryCredentials("dockerhub")
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
